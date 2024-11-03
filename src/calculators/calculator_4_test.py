91de0f6959c56547d2a17f258eb36bba3ff0315d
Ana Ventura <anaventura1811@gmail.com>
2024-11-03 19:54:35 -0300

    feat: add unit tests for class calculator 4

---
+++ src/calculators/calculator_4_test.py
@@ -0,0 +1,26 @@
+from src.calculators.calculator_4 import Calculator4
+from typing import Dict
+from pytest import raises
+
+
+class MockRequest:
+    def __init__(self, body: Dict) -> None:
+        self.json = body
+
+
+def test_calculate():
+    mock_request = MockRequest(body={"numbers": [1, 2, 3, 4]})
+    calc = Calculator4()
+    response = calc.calculate(mock_request)
+    assert "data" in response
+    assert "Calculator" in response["data"]
+    assert "value" in response["data"]
+    assert response == {'data': {'Calculator': 4, 'value': 2.5}}
+
+
+def test_calculator_error():
+    mock_request = MockRequest(body={"teste": 1})
+    calc = Calculator4()
+    with raises(Exception) as infoexecpt:
+        calc.calculate(mock_request)
+    assert str(infoexecpt.value) == 'body mal formatado!'
