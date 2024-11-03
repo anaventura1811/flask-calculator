60caae5cd8e88b26b9e2287d5db5dce01f048b36
Ana Ventura <anaventura1811@gmail.com>
2024-11-03 13:51:02 -0300

    feat: add tests to calculator class, add vscode settings to project

--- src/calculators/calculator_1_test.py
+++ src/calculators/calculator_1_test.py
@@ -1,8 +1,23 @@
-def test_calculator_1():
-    print('\nEstou aqui no teste 1')
-    pass
+from typing import Dict
+from .calculator_1 import Calculator1
 
 
-def test_calculator_2():
+class MockRequest:
+    def __init__(self, body: Dict) -> None:
+        self.json = body
+
+
+def test_calculate():
+    mock_request = MockRequest(body={"number": 1})
+    calculator1 = Calculator1()
+    response = calculator1.calculate(mock_request)
+    assert "data" in response
+    assert "Calculator" in response["data"]
+    assert "result" in response["data"]
+    assert response["data"]["result"] == 14.25
+    assert response["data"]["Calculator"] == 1
+
+
+def test_calculate_with_body_error():
     print('\nEstou aqui no teste 2')
     pass
