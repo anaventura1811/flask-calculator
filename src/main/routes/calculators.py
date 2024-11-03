6e53a54d1dab16d2103dff8c571866238da70eaf
Ana Ventura <anaventura1811@gmail.com>
2024-11-03 16:12:05 -0300

    refactor: add factories folder, move calculator classes to factories folder

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,22 +1,19 @@
 from flask import Blueprint, jsonify, request
-from src.calculators.calculator_1 import Calculator1
-from src.calculators.calculator_2 import Calculator2
-from src.drivers.numpy_handler import NumpyHandler
-
+from src.main.factories.calculator_2_factory import calculator2_factory
+from src.main.factories.calculator_1_factory import calculator1_factory
 
 calc_route_bp = Blueprint("calc_routes", __name__)
 
 
 @calc_route_bp.route('/calculator/1', methods=['POST'])
 def calculator_1():
-    calc = Calculator1()
+    calc = calculator1_factory()
     response = calc.calculate(request)
     return jsonify(response)
 
 
 @calc_route_bp.route('/calculator/2', methods=['POST'])
 def calculator_2():
-    driver = NumpyHandler()
-    calc = Calculator2(driver_handler=driver)
+    calc = calculator2_factory()
     response = calc.calculate(request)
     return jsonify(response)
